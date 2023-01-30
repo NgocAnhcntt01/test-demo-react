dae45b13a5748de92f31f90eda07b73c2a41ac5e
NgocAnhcntt01 <NgocAnhcntt01@users.noreply.github.com>
2023-01-30 09:43:23 +0700

    video20

--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -6,10 +6,24 @@ class MyComponent extends React.Component{
         name : 'Ngoc Anh',
         address: 'Thai Nguyen',
         age: 23
+    };
+    handleClick = (event) => {
+        console.log('My name is ', this.state.name)
+
+        this.setState({
+            name: 'Ngoc Mai',
+            age: Math.floor((Math.random()*100) + 1)
+        })
+    }
+    handleOnMouseClick() {
+        console.log('Hello123')
     }
     render(){
         return(
-            <div>My name is {this.state.name} and I'm from {this.state.address}</div>
+            <div>My name is {this.state.name} and I'm {this.state.age}
+            <button onClick={(event) => {this.handleClick(event)}}>Click me</button>
+            <button onMouseOver={this.handleOnMouseClick}>Hover me</button>
+            </div>
         );
     }
 }
