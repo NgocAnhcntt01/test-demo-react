43a94a315b5722a9a46cf202ab02f9d0be4dc66b
NgocAnhcntt01 <NgocAnhcntt01@users.noreply.github.com>
2023-02-03 08:02:29 +0700

    video46

--- src/components/DisplayInfor.js
+++ src/components/DisplayInfor.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import './DisplayInfor.scss';
 import logo from '../logo.svg';
 
@@ -34,38 +34,40 @@ import logo from '../logo.svg';
 //     }
 // }
 
-const DisplayInfor = (props) =>{
+const DisplayInfor = (props) => {
     const { listUsers } = props;
 
-    const[isShowHideListUser, setShowHideListUser] = useState(true)
+    const [isShowHideListUser, setShowHideListUser] = useState(true)
 
     const handleShowHideListUser = () => {
         setShowHideListUser(!isShowHideListUser)
     }
-        return (
-            <div className='display-infor-container'>
-                <button onClick={() => handleShowHideListUser()}>{isShowHideListUser === true ? 'Hide list users' : 'Show list user'}</button>
-                {isShowHideListUser &&
-                    <>
-                        {listUsers.map((user, index) => {
-                            return (
-                                <div key={user.id} className={+user.age > 20 ? "red" : "green"}>
-                                    <div>
-                                        <div>My name's {user.name}</div>
-                                        <div>My age's {user.age}</div>
-                                    </div>
-                                    <div>
-                                        <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
-                                    </div>
-
-                                    <hr />
+    useEffect(() => { console.log('aaav') },[])
+    console.log('aaa')
+    return (
+        <div className='display-infor-container'>
+            <button onClick={() => handleShowHideListUser()}>{isShowHideListUser === true ? 'Hide list users' : 'Show list user'}</button>
+            {isShowHideListUser &&
+                <>
+                    {listUsers.map((user, index) => {
+                        return (
+                            <div key={user.id} className={+user.age > 20 ? "red" : "green"}>
+                                <div>
+                                    <div>My name's {user.name}</div>
+                                    <div>My age's {user.age}</div>
                                 </div>
-                            )
-                        })}
-                    </>
-                }
-            </div>
-        )
+                                <div>
+                                    <button onClick={() => props.handleDeleteUser(user.id)}>Delete</button>
+                                </div>
+
+                                <hr />
+                            </div>
+                        )
+                    })}
+                </>
+            }
+        </div>
+    )
 }
 
 export default DisplayInfor;
--- src/components/Mycomponent.js
+++ src/components/Mycomponent.js
@@ -62,6 +62,7 @@ const MyComponent = (props) =>{
     return (
             <>
                 <div>
+                <p>Các bạn vui lòng Inbox qua Fanpage Facebook Hỏi Dân IT:&nbsp; facebook(chấm)com/askITwithERIC</p><p>để được hướng dẫn cài đặt dự án Backend.</p><p><strong>Cú pháp tin nhắn:</strong></p><p>UDEMY_IDENTIFY = &lt;điền_vào_email (hoặc số điện thoại) của tài khoản dùng để đăng nhập Udemy&gt;</p><p>UDEMY_NAME = &lt;điền_vào_tên_hiển_thị_của_bạn_trên_Udemy&gt;</p><p>NỘI_DUNG = REACT-ULTIMATE-HƯỚNG-DẪN-CÀI-ĐẶT-BACKEND</p><p><strong>Ví dụ:</strong></p><p>UDEMY_IDENTIFY = eric@gmail.com</p><p>UDEMY_NAME = Hỏi Dân IT</p><p>NỘI_DUNG = REACT-ULTIMATE-HƯỚNG-DẪN-CÀI-ĐẶT-BACKEND</p><p>Trường hợp các bạn dùng Google/Facebook để đăng nhập Udemy, thì các bạn ghi giúp mình tên đăng nhập Google/Facebook vào phần udemy_identify</p>
                     <AddUserInfor
                         handleAddNewUser={handleAddNewUser}
                     />
